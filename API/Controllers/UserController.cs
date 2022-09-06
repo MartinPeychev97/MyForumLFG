f84ec1a0405631fce2bb987bc509a0e77c94f640
martin.peychev97@gmail.com <martin.peychev97@gmail.com>
2022-09-06 20:37:37 +0300

    Finished UserController

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,15 +1,55 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API.Services.ServiceContracts;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Services.Requests;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
-    {
-        public UserController()
-        {
+    private IUserService _userService;
+    private IMapper _mapper;
 
-        }
+    public UserController(IUserService userService, IMapper mapper)
+    {
+        _userService = userService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var users = _userService.GetAll();
+        return Ok(users);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(Guid id)
+    {
+        var user = _userService.GetById(id);
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public IActionResult Create(CreateUserRequest model)
+    {
+        _userService.Create(model);
+        return Ok(new { message = "User created" });
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(Guid id, UpdateUserRequest model)
+    {
+        _userService.Update(id, model);
+        return Ok(new { message = "User updated" });
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        _userService.Delete(id);
+        return Ok(new { message = "User deleted" });
     }
 }
